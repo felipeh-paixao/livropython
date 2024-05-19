cec82f0392adb41b3c5ed5c626ff5022638abfd3
felipeh-paixao <131300973+felipeh-paixao@users.noreply.github.com>
2024-05-19 22:16:46 +0000

    recomeçando novamente

---
+++ Chapter_2/ex2.8.py
@@ -0,0 +1,6 @@
+print(5 + 3)
+print(11 - 3)
+print(16 // 2)
+print(4 * 2)
+
+#Aqui testamos como funciona as 4 operações principais em Python
---
+++ Chapter_2/ex2.9.py
@@ -0,0 +1,5 @@
+num_favorito = 25
+
+#Usamos a função str para converter o inteiro em string para não ter erro na hora de imprimir
+message = 'O dia ' + str(num_favorito) + ' é meu favorito porque nele recebo o salário'
+print(message)
