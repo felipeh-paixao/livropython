bd2748e435291ec0e45eab982c91b6c83eb54b74
felipeh-paixao <131300973+felipeh-paixao@users.noreply.github.com>
2024-05-22 08:55:37 +0000

    vou concluir este livro

---
+++ Chapter_3/ex3.8.py
@@ -0,0 +1,19 @@
+#Conhecendo o mundo
+
+places = ['Tailândia', 'Japão', 'Estados Unidos', 'Israel']
+
+print(places)
+print()
+print(sorted(places))
+print(places)
+print()
+print(sorted(places,reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
---
+++ Chapter_3/ex3.9.py
@@ -0,0 +1,4 @@
+#Convidados para o jantar
+convidados = ['Jéssica', 'Mãe', 'Di']
+
+print(f'Bom dia, a noite receberei {len(convidados)} convidados em casa')
